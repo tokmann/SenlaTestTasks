fdc79f79303ca330591c28754f4b2e00901bb8a1
rodion <rodotokma@gmail.com>
2025-09-23 17:22:45 +0300

    Fixed password generator

--- src/main/java/password_generator/Main.java
+++ src/main/java/password_generator/Main.java
@@ -2,7 +2,7 @@ package password_generator;
 
 public class Main {
     public static void main(String[] args) {
-        passwordgenerator.PasswordGenerator generator = new passwordgenerator.PasswordGenerator();
+        PasswordGenerator generator = new PasswordGenerator();
         generator.startGenerator();
     }
 }
